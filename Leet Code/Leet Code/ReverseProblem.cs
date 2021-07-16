f2633a51a2b4374e2b9e36d807d5fd2dbf71acd4
Nick May <nickmay645@gmail.com>
2021-07-16 13:03:55 -0400

    reverse problem

---
+++ Leet Code/Leet Code/ReverseProblem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Leet_Code
+{
+    class ReverseProblem
+    {
+        public int Reverse(int x)
+        {
+
+            string num = x.ToString();
+
+        }
+
+    }
+}
