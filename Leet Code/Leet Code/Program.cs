5a4385e3c9086c2c0cc68cc2bf75352c912f9059
Nick May <nickmay645@gmail.com>
2021-07-16 11:18:39 -0400

    Adding C# solution and files

---
+++ Leet Code/Leet Code/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Leet_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program prog = new Program();
+            
+            // Testing Variables and Code
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] a = prog.TwoSum(nums, target);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /*
+        Given an array of integers nums and an integer target, 
+        return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution, 
+        and you may not use the same element twice.
+
+        You can return the answer in any order.
+         */
+        public int[] TwoSum(int[] nums, int target)
+        {
+        for(int i = 0; i<nums.Length; i++)
+            {
+                for(int j = i+1; j<nums.Length; j++)
+                {
+                    if(nums[j] == target - nums[i])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+        throw new ArgumentException("Not found");
+        }
+    
+    }
+}
+
+
